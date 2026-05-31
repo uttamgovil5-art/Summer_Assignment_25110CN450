c4cbc61647467f3ec7dab7b088afc44a460a95aa
UTTAM GOVIL <uttamgovil5@gmail.com>
2026-05-31 09:52:43 +0530

    Delete Day-1 directory

--- Day-1/sum.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class sum {
-  public static void main(String args[]) {
-    Scanner sc = new Scanner (System.in);
-  int N = sc.nextInt();
-  int sum = (N*(N+1))/2;
-  System.out.println("Sum of natural num =" + sum);
-  }
-}
